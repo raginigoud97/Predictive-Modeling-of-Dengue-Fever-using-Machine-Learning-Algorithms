110711976a69cb87008935e5d6f33167dcee28e3
raginigoud97 <129501243+raginigoud97@users.noreply.github.com>
2024-11-01 09:16:20 +0530

    Add files via upload

---
+++ Dengu Disease Prediction/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Dengue_Disease_Predection project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dengue_Disease_Predection.settings')
+
+application = get_asgi_application()
